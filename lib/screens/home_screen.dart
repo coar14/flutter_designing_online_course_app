07b8f692e39badacbe4c245ae62853aface762cf
cor <m.masong14@gmail.com>
2023-09-25 08:19:39 +0800

    added graphic design class

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_designing_online_course_app/widgets/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -50,14 +50,54 @@ class _HomeScreenState extends State<HomeScreen> {
                   scrollDirection: Axis.horizontal,
                   physics: const BouncingScrollPhysics(),
                   itemBuilder: (context, index) {
-                    return const HorizontalList(
-                        startColor: 0xFF9288E4,
-                        endColor: 0xFF534EA7,
-                        courseHeadLine: 'Recommeded',
-                        courseTitle: 'UI/UX DESINGER \n BEGINNER',
-                        courseImage: 'assets/images/img_saly10.png');
+                    if (index == 0) {
+                      return const HorizontalList(
+                          startColor: 0xFF9288E4,
+                          endColor: 0xFF534EA7,
+                          courseHeadLine: 'Recommeded',
+                          courseTitle: 'UI/UX DESINGER \n BEGINNER',
+                          courseImage: 'assets/images/img_saly10.png');
+                    } else if (index == 1) {
+                      return const HorizontalList(
+                          startColor: 0xFFF4C465,
+                          endColor: 0xFFC63956,
+                          courseHeadLine: 'NEW CLASS',
+                          courseTitle: 'GRAPHIC\nDESIGN',
+                          courseImage: 'assets/images/img_saly36_detail.png');
+                    }
                   }),
-            )
+            ),
+            const SizedBox(
+              height: 34,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Free online class',
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
+                Text(
+                  'From over 80 lectures',
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w500,
+                    color: const Color(0xFF9C9A9A),
+                    fontSize: 14,
+                  ),
+                )
+              ],
+            ),
+            ListView.builder(
+                itemCount: 5,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Container();
+                })
           ],
         ),
       ),
